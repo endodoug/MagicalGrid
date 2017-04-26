0b655d18ab9affb852ce494e7777a28458b625f5
Doug Harper <doug@zewkey.com>
2017-04-25 22:57:55 -0500

    Added a playground to the workspace Added file > new >t arget > iOS > Framework&Library > Cocoa Touch Framework

---
+++ MagicalGridiOS/MagicalGridiOS.h
@@ -0,0 +1,19 @@
+//
+//  MagicalGridiOS.h
+//  MagicalGridiOS
+//
+//  Created by doug harper on 4/25/17.
+//  Copyright © 2017 Doug Harper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagicalGridiOS.
+FOUNDATION_EXPORT double MagicalGridiOSVersionNumber;
+
+//! Project version string for MagicalGridiOS.
+FOUNDATION_EXPORT const unsigned char MagicalGridiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicalGridiOS/PublicHeader.h>
+
+
