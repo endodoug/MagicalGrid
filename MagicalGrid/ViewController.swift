f4db10a9239af99006de92b90c070fa8f78bd586
Doug Harper <doug@zewkey.com>
2017-04-25 23:15:49 -0500

    Added a simple red box to get started.

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    let redView = UIView()
+    redView.backgroundColor = .red
+    redView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    view.addSubview(redView)
   }
 
 }
